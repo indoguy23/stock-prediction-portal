99217be330221c4349abee25bd8004143ef1896d
Aryan Dewangan <dewanganbob11@gmail.com>
2025-08-18 15:59:19 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./assets/css/style.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Button from "./Button";
+
+const Main = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="p-5 text-center bg-light-dark rounded">
+          <h1 className="text-light">Stock Prediction Portal</h1>
+          <p className="text-light lead">
+            A Stock Prediction Application is a software tool that uses
+            historical market data, statistical models, and machine learning
+            techniques to analyze stock trends and forecast future prices. It
+            helps investors and traders make informed decisions by providing
+            insights into market movements, risk analysis, and potential
+            investment opportunities in real time.
+          </p>
+          <Button text="Login" class="btn btn-outline-info"/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
